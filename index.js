af09c8890aba80ff3234206bd2ed71d7c613a89f
Aijaz <geeksen7@gmail.com>
2025-09-07 04:24:11 +0530

    Added Zod validation to heartInputValidation

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const zod = require('zod');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const port = 3007;
@@ -69,31 +71,20 @@ function kidneyInputValidation(req, res, next) {
 }
 
 function heartInputValidation(req, res, next) {
-    const heartData = req.body;
-    const bpm = heartData.bpm;
-    const heartRate = heartData.heartRate;
-    if(!bpm) {
-        const err = createError('bpm input is missing', 400);
-        console.log(err.message+" "+" "+err.status)
+    const heartSchema = zod.object( {
+        bpm : zod.number('bpm input is missing').min(60, 'The bpm input should be between 60 and 100').max(100, 'The bpm input should be between 60 and 100'),
+        heartRate : zod.number('hearRate is missing').min(30, 'The heartRate should be between 30 and 100').max(100, 'The heartRate should be between 30 and 100')
+    });
+    const heartResult = heartSchema.safeParse(req.body);
+    console.log(heartResult);
+    if(!heartResult.success) {
+        const errorMessages = heartResult.error.issues.map((issue) => issue.message);
+        const err = createError('Validaton failed', 400);
+        err.details = errorMessages;
         return next(err);
     }
-    if(!heartRate) {
-        const err = createError('heartRate input is missing', 400);
-        console.log(err.message+" "+" "+err.status)
-        return next(err);
-    }
-    if(bpm < 60 || bpm > 100) {
-        const err = createError('bpm input should be between range 60 and 100', 400);
-        console.log(err.message+" "+" "+err.status)
-        return next(err);
-    }
-    if(heartRate < 30 || heartRate > 100) {
-        const err = createError('heartRate input should be between range 60 and 100', 400);
-        console.log(err.message+" "+" "+err.status)
-        return next(err);
-    }
-
-    return next();
+    req.validatedHeartData = heartResult.data;
+    return next ();
 }
 
 
@@ -111,10 +102,7 @@ app.post('/kidney', kidneyInputValidation, (req, res) => {
 });
 
 app.post('/heart', heartInputValidation, (req, res) => {
-    const heartData = req.body;
-    const bpm = heartData.bpm;
-    const heartRate = heartData.heartRate;
-
+    const { bpm, heartRate } = req.validatedHeartData;
     if(bpm > 60 && bpm < 80) {
         if(heartRate > 60 && heartRate < 80) {
             return res.status(200).json( {
@@ -138,7 +126,8 @@ app.post('/heart', heartInputValidation, (req, res) => {
 app.use((err, req, res, next) => {
     res.status(err.status || 500).json( {
         message : err.message || 'Internal Server Error',
-        status : 'failed'
+        status : 'failed',
+        details : err.details || []
     });
 }
     

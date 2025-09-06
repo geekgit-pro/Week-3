b683b34c8c2c74d752bee9cf64b71626cebfcfae
Aijaz <geeksen7@gmail.com>
2025-09-06 14:43:19 +0530

    Added kidneyInputValidation & heartInputValidation with refined with global error handler

--- index.js
+++ index.js
@@ -8,68 +8,115 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+function createError (message, status) {
+    const err = new Error(message);
+    err.status = status;
+    return err;
+}
+
 function userValidation(req, res, next) {
     const username = req.headers['user'];
     const password = req.headers['password'];
     if(!username || !password) {
-        return res.status(400).json( {
-            message : 'User name or password is missing',
-            status : 'failed'
-        });
+        const err = createError('Username or password is missing', 400);
+        return next(err);
     }
 
     if(username != 'admin' || password != 'admin') {
-        return res.status(401).json( {
-            message : 'User not authorized',
-            status : 'failed'
-        });
+        const err = createError('User not authorized', 401);
+        return next(err);
     }
     return next();
 }
 
-app.use(userValidation);
-
-app.post('/kidney', (req, res) => {
+function kidneyInputValidation(req, res, next) {
     const kidneyData = req.body;
     const noOfKidneys = kidneyData.noOfKidneys;
     const phValue = kidneyData.phValue;
+
     if(!noOfKidneys) {
-        return res.status(400).send('No of kidneys data invalid');
+        const err = new Error('No of kidneys is missing');
+        err.status = 400;
+        console.log(err.status+"  "+err.message);
+        return next(err);
     }
-
-    if(noOfKidneys != 2)
-        return res.status(400).send('No of kidneys should be 2');
-
-    if(noOfKidneys == 2 && !phValue) {
-        return res.status(400).send('Ph value is required when no of kidneys is 2');
+    if(noOfKidneys != 2){
+        const err = new Error('No of kidneys should be 2');
+        err.status = 400;
+        console.log(err.status+"  "+err.message);
+        return next(err);
     }
-
-    if(noOfKidneys == 2 && (phValue.length != 2)) {
-        return res.status(400).send('Ph value should be an array of length 2');
+        
+    if(!phValue) {
+        const err = new Error('phValue is missing');
+        err.status = 400;
+        console.log(err.status+"  "+err.message);
+        return next(err);
     }
-
+    if(phValue.length != 2) {
+        const err = new Error('phValue is missing some information');
+        err.status = 400;
+        console.log(err.status+"  "+err.message);
+        return next(err);
+    }
     if(noOfKidneys == 2 && (phValue[0] < 4 || phValue[0] > 8 || phValue[1] < 4 || phValue[1] > 8)) {
-        return res.status(400).send('Ph value should be between 4 and 8');
+        const err = new Error('phValue should be between 4 and 8');
+        err.status = 400;
+        console.log(err.status+"  "+err.message);
+        return next(err);
+    }
+    return next();
+}
+
+function heartInputValidation(req, res, next) {
+    const heartData = req.body;
+    const bpm = heartData.bpm;
+    const heartRate = heartData.heartRate;
+    if(!bpm) {
+        const err = createError('bpm input is missing', 400);
+        console.log(err.message+" "+" "+err.status)
+        return next(err);
+    }
+    if(!heartRate) {
+        const err = createError('heartRate input is missing', 400);
+        console.log(err.message+" "+" "+err.status)
+        return next(err);
+    }
+    if(bpm < 60 || bpm > 100) {
+        const err = createError('bpm input should be between range 60 and 100', 400);
+        console.log(err.message+" "+" "+err.status)
+        return next(err);
+    }
+    if(heartRate < 30 || heartRate > 100) {
+        const err = createError('heartRate input should be between range 60 and 100', 400);
+        console.log(err.message+" "+" "+err.status)
+        return next(err);
     }
 
+    return next();
+}
+
+
+app.use(userValidation);
+
+app.post('/kidney', kidneyInputValidation, (req, res) => {
+    const kidneyData = req.body;
+    const noOfKidneys = kidneyData.noOfKidneys;
+    const phValue = kidneyData.phValue;
+    
     res.status(200).json( {
         message : `ph Vlaue for kidney 1 is ${phValue[0]} and for kidney 2 is ${phValue[1]}`,
         status : 'success'
     });
 });
 
-app.post('/heart', (req, res) => {
+app.post('/heart', heartInputValidation, (req, res) => {
     const heartData = req.body;
     const bpm = heartData.bpm;
     const heartRate = heartData.heartRate;
-    if(!bpm) {
-        return res.status(400).send('BPM data invalid');
-    }
-    if(!heartRate) {
-        return res.status(400).send('Heart Rate data invalid');
-    }
-    if(bpm > 60 && bpm < 100) {
-        if(heartRate > 60 && heartRate < 100) {
+
+    if(bpm > 60 && bpm < 80) {
+        if(heartRate > 60 && heartRate < 80) {
             return res.status(200).json( {
                 message : `BPM is ${bpm} and Heart Rate is ${heartRate}`,
                 status : 'success'
@@ -86,6 +133,16 @@ app.post('/heart', (req, res) => {
         status : 'success'
     });
 });
+
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json( {
+        message : err.message || 'Internal Server Error',
+        status : 'failed'
+    });
+}
+    
+)
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
